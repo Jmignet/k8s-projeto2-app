eb11a60b220493b11ffcaee1414eb8bf00506514
Miguel Manzanares <jmanzanares@clarian.com.br>
2022-12-24 03:43:24 -0300

    Delete dockerfile nao utilizado e ajustes no script principal

--- script.bat
+++ script.bat
@@ -1,20 +1,14 @@
-kubectl apply -f .\mysql-deployment.yml --record
-kubectl apply -f .\app-deployment.yml --record
 
-echo "Processando imagens..."
+echo "Processando imagem..."
+docker build -t docker-hub-user/k8s-projeto2-app:1.0 app/.
 
-docker build -t docker-hub-user/k8s-projeto2-app-backend:1.0 backend/.
-docker build -t docker-hub-user/k8s-projeto2-app-database:1.0 database/.
+echo "Fazendo upload da imagem..."
+docker push docker-hub-user/k8s-projeto2-app:1.0
 
-echo "Fazendo upload das imagens..."
-
-docker push docker-hub-user/k8s-projeto2-app-backend:1.0
-docker push docker-hub-user/k8s-databas2-app-database:1.0
-
-echo "Criando serviço no cluster k8s..."
-
-kubectl apply -f ./services.yml
+echo "Lendo secrets..."
+kubectl apply -f ./secrets.yml
+rm ./secrets.yml
 
 echo "Realizando deployment no cluster k8s..."
-
-kubectl apply -f ./deployment.yml
+kubectl apply -f .\mysql-deployment.yml --record
+kubectl apply -f .\app-deployment.yml --record
